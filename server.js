c653a558fc459f614d1638e7012ddd264f95c4d1
winthaic <winthaic@hotmail.com>
2024-11-06 08:12:31 +0000

    push project

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('express');
+const PORT = 8000;
+const HOST = '0.0.0.0';
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('Hello World - 1.0.0');
+});
+
+app.listen(PORT,HOST,() => {
+    console.log(`APP Running on http://${HOST}:${PORT}`);
+});
