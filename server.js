4c2e8e112ddd56a04415bc2aeab291452da27e90
winthaic <winthaic@hotmail.com>
2024-11-06 08:49:33 +0000

    tttt

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('Hello World - 1.1.2 testtest');
+    res.send('Hello World - 1.1.2 testtessst');
 });
 
 app.listen(PORT,HOST,() => {
